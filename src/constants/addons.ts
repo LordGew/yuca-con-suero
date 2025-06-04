c9aec017461d24af51b0a154a1bb373e677b22d1
Jairo Hernández <55113454+LordGew@users.noreply.github.com>
2025-06-03 23:19:03 -0400

    ajustes visuales

--- src/constants/addons.ts
+++ src/constants/addons.ts
@@ -14,26 +14,10 @@
 */
 export const addonsAvailable = [
     {
-        image: "https://bnetcmsus-a.akamaihd.net/cms/blog_header/it/IT3NWTCQWD821613777910714.jpg",
-        CardTitle: "Addon Control Panel",
-        titleHref: "https://foro.ultimowow.com/files/file/10-addon-control-panel/?do=download&csrfKey=1c891ffb7678e360d23453dc7e50fc87",
-        btnHref: "https://foro.ultimowow.com/files/file/10-addon-control-panel/?do=download&csrfKey=1c891ffb7678e360d23453dc7e50fc87",
-        CardDescription: "Te permite administrar tus addons en el juego, con una interfaz muy cómoda.",
-        Button: "Descargar addons"
-    },
-    {
-        image: "https://cl2.buscafs.com/www.levelup.com/public/uploads/images/841103/841103.jpg",
-        CardTitle: "Mas addons",
-        titleHref: "https://www.curseforge.com/wow",
-        btnHref: "https://www.curseforge.com/wow",
-        CardDescription: "Busca y descarga más addons desde la página oficial de CurseForge.",
-        Button: "Ir a CurseForge"
-    },
-    {
         image: "https://static.wixstatic.com/media/5dd8a0_8c5b7a3a15014ad599ca7a0d6d6dfa35~mv2.jpg",
         CardTitle: "Launcher",
-        titleHref: "",
-        btnHref: "",
+        titleHref: "https://www.mediafire.com/file/vrjxkd7hugh2x7x/Entropiux_Launcher.rar/file",
+        btnHref: "https://www.mediafire.com/file/vrjxkd7hugh2x7x/Entropiux_Launcher.rar/file",
         CardDescription: "Accede a tu cuenta de juego y descarga el cliente de juego desde aquí.",
         Button: "Descargar"
     }
